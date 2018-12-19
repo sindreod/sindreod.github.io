acdaa1c3b61876b937cf543ef8c1099e30e55a8e
Sindre Deisz <sindreod@stud.ntnu.no>
2018-12-19 03:48:22 +0100

    Double tab menu

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,16 +1,16 @@
 //Tab menu function
-function openTab(evt, tabId) {
+function openTab(evt, tabId, tabcontentString, tablinkString) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName(tabcontentString);
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
+    // Get all elements with class="tablinkString" and remove the class "active"
+    tablinks = document.getElementsByClassName(tablinkString);
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
